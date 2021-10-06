5ab275bcdaa7f1895e4aec33464a2976536dae9d
Bruna Amaral <bamaralqa@gmail.com>
2021-10-06 17:50:11 -0300

    Exclui o "Bom dia!!!" no começo da impressão de texto do Console
    Criei o método calcularAreaModoCurto e movi o código que estava em main para lá
    
    O método main agora chama os 2 calcular* que passaram a ser static

--- src/main/java/intro/Medidas.java
+++ src/main/java/intro/Medidas.java
@@ -4,27 +4,41 @@ package intro;
 // 2 - Referência as Bibliotecas
 
 // 3 - Classe
-
 public class Medidas {
     // 3.1 - Atributos - Caracteristicas
-    public static void main(String[] args)
-    {
-        System.out.println("Bom dia!!!");
-        System.out.println("Cálculo de áreas");
 
+    // 3.2 - Métodos e Funções
+    public static void main(String[] args) {
+        calcularAreaModoCurto();
+        calcularAreaModoExtenso();
+
+    }
+
+    public static void calcularAreaModoCurto() {
+        System.out.println("Cálculo de áreas Modo Curto:");
+
+        // Calculo de Area - Reduzido
+        int largura = 4;
+        int comprimento = 3;
+
+        System.out.println("Para a largura de " + largura + "m e o comprimento de " + comprimento +
+                "m a área é de " + largura * comprimento + "m²");
+    }
+
+    public static void calcularAreaModoExtenso() {
+        System.out.println("Cálculo de áreas Modo Extenso:");
         // Calculo de Area -  Exemplo: O tamanho do tapete o do piso
+
         int largura;
         int comprimento;
         int resultado;
 
-        largura = 8;
+        largura = 4;
         comprimento = 3;
 
         resultado = largura * comprimento;
         System.out.println("Para a largura de " + largura + "m e o comprimento de " + comprimento +
                 "m a área é de " + resultado + "m²");
-        // 3.2 - Métodos e Funções
     }
 }
-
 
