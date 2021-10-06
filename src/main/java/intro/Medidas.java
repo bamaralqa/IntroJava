bb5bb3795a729d62cfb5f5a2fc604848b04cc342
Bruna Amaral <bamaralqa@gmail.com>
2021-10-06 15:15:49 -0300

    Inclui o "Bom dia!!!" no começ da impressão de texto do Console

---
+++ src/main/java/intro/Medidas.java
@@ -0,0 +1,30 @@
+// 1 - Pacote
+package intro;
+
+// 2 - Referência as Bibliotecas
+
+// 3 - Classe
+
+public class Medidas {
+    // 3.1 - Atributos - Caracteristicas
+    public static void main(String[] args)
+    {
+        System.out.println("Bom dia!!!");
+        System.out.println("Cálculo de áreas");
+
+        // Calculo de Area -  Exemplo: O tamanho do tapete o do piso
+        int largura;
+        int comprimento;
+        int resultado;
+
+        largura = 8;
+        comprimento = 3;
+
+        resultado = largura * comprimento;
+        System.out.println("Para a largura de " + largura + "m e o comprimento de " + comprimento +
+                "m a área é de " + resultado + "m²");
+        // 3.2 - Métodos e Funções
+    }
+}
+
+
